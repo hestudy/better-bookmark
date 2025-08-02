872962e263e6b1445d48a39ac79f581d9805c593
hestudy <1467602180@qq.com>
2025-08-02 23:15:51 +0800

    ✨ 重构数据库模型并集成 better-auth，支持用户、会话、账户与验证

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,17 @@
+import { auth } from "@/lib/auth.js";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import bookmarksRouter from "./routes/bookmarks.js";
+import { showRoutes } from "hono/dev";
 
 const app = new Hono()
   .get("/", (c) => {
     return c.text("Hello Hono!");
   })
+  .on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw))
   .route("/bookmarks", bookmarksRouter);
+
+showRoutes(app);
 
 serve(
   {
